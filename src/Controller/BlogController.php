e3c9795fde81b62d512ddaf6b29ab2c8d4d74652
clsdjo30 <lesergent.cedric@gmail.com>
2023-01-31 04:09:03 +0100

    feat(user): install paginator - install htmx - add path blog & blog/id - add & create blog templates

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use App\Repository\TagRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/blog')]
+class BlogController extends AbstractController
+{
+    #[Route('/', name: 'blog')]
+    public function index(
+        PostRepository $postRepository,
+        TagRepository $tag,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response
+    {
+        $data = $postRepository->findAll();
+
+        $posts = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            5
+        );
+
+        return $this->render('blog/index.html.twig', [
+            'posts' => $posts,
+            'lasts' => $postRepository->findByCreatedAt(),
+            'tags' => $tag->findAll()
+        ]);
+    }
+
+    #[Route('/{id}', name: 'post_details', methods: ['GET'])]
+    public function showAnnounceDetails(
+        Post $post,
+    ): Response {
+        return $this->render('blog/show_post_details.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+
+
+}
