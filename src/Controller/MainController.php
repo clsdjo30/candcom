c419cb644a8920a8f3b4b40e43cdff014d3e2045
clsdjo30 <lesergent.cedric@gmail.com>
2023-02-12 18:40:03 +0100

    feat(contact): create Contact Entity make migration, ContactType.php, - add form template in MainController.php - Create MailerService.php - modify MainController.php - add csrf form protection

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,22 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use App\Repository\PostRepository;
+use App\Service\MailerService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class MainController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/', name: 'main')]
-    public function index(PostRepository $postRepository): Response
+    public function index(
+        PostRepository     $postRepository,
+        Request            $request,
+        MailerService      $mailer,
+    EntityManagerInterface $manager
+    ): Response
     {
         $posts = $postRepository->findAll();
 
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // On recupere les données du formulaire
+            $contactFormData = $form->getData();
+
+             // On injecte les données du formulaire dans le Mailer Service
+            $subject = 'Demande de contact sur votre site de ' . $contactFormData->getEmail();
+            $content = $contactFormData->getName() . ' vous a envoyé le message suivant: ' . $contactFormData->getMessage();
+
+            // On envoi le formulaire par mail
+            $mailer->sendEmail(subject: $subject, content: $content);
+
+            //On sauvegarde les données en BDD
+            $manager->persist($contactFormData);
+            $manager->flush();
+
+            $this->addFlash('success', 'Votre message a bien été envoyé, nous le traiterons dans les meilleurs délais !');
+
+            return $this->redirectToRoute('main');
+        }
 
         return $this->render('main/index.html.twig', [
-        'posts' => $posts
+            'posts' => $posts,
+            'form' => $form->createView()
+
 
         ]);
     }
+
+
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',TextType::class)
+            ->add('email',EmailType::class)
+            ->add('message', TextareaType::class)
+            ->add('isAgree', CheckboxType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token'
+
+        ]);
+    }
+}
